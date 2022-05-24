57ad5b018a38f4cd2c17e33d2a58cac274f581d0
xanhacks <xanhacks@protonmail.com>
2022-05-24 20:17:05 +0200

    heist in prod

--- Prog/heist/entry.sh
+++ Prog/heist/entry.sh
@@ -2,5 +2,5 @@
 
 while :
 do
-    su -c "exec socat TCP-LISTEN:7001,reuseaddr,fork EXEC:'/heist/chall.py,stderr'" - player;
+    su -c "exec socat TCP-LISTEN:${LISTEN_PORT},reuseaddr,fork EXEC:'/heist/chall.py,stderr'" - player;
 done
