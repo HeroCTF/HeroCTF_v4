c23b3b0407df0ce1a78a36e7af009ba9471d43ef
Log_s <leo951206@gmail.com>
2022-05-28 11:57:53 +0200

    fix pyjail

--- Misc/pyjAil_iS_Mad/pyjail_redacted.py
+++ Misc/pyjAil_iS_Mad/pyjail_redacted.py
@@ -18,6 +18,10 @@ def jail():
             valid_input = False
             break
     
+    if not all(ord(c) < 128 for c in user_input):
+        valid_input = False
+        print("That's not ascii mate")
+    
     if valid_input:
         try:
             exec(user_input, {"__builtins__": {}}, {'flag':flag})
